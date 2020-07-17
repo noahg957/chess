2ec8ed282c3ee348baabb569db4a55d91f11d967
Noah Gluck <noahgluck@gmail.com>
2020-07-17 16:58:32 -0700

    added display method to square class

---
+++ lib/square.rb
@@ -0,0 +1,24 @@
+require 'colorize'
+
+class Square
+  
+  attr_reader :position, :color
+  attr_accessor :occupied, :occupying_piece
+
+  def initialize(column, row, color)
+    @position = [column, row]
+    @occupied = false
+    @occupying_piece = nil
+    @color = color
+  end
+
+  def display
+    if @occupying_piece.nil?
+      "   ".colorize(background: "light_#{@color}".to_sym)
+    else
+      #will need to change to occupying_piece.display once i make the pieces.
+    " #{@occupying_piece} ".colorize(background: "light_#{@color}".to_sym)
+    end
+  end
+
+end
